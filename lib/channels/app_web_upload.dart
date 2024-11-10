7ac3bd8efea4150c102f239e5f5bd26176d8e86b
devcone-one <esa12096@gmail.com>
2024-11-10 12:02:26 +0900

    6546545464

--- lib/channels/app_web_upload.dart
+++ lib/channels/app_web_upload.dart
@@ -70,7 +70,7 @@ extension AppWebUpload on AppWebChannel {
         body: fileContent
       );
         if (response.statusCode == 200) {
-          print("fdsfdsfsd ${response.statusCode},  ${response.body}");
+          print("df343243243 ${response.statusCode},  ${response.body}");
           if(onSuccess != null) {
             onSuccess();
           }
