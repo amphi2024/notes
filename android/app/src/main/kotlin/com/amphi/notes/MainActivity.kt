74bcc42b05e2847057bb7d0e8c285088c90f38ce
amphi2024 <amphiprioninae2024@gmail.com>
2025-01-03 12:58:03 +0900

    commit

--- android/app/src/main/kotlin/com/amphi/notes/MainActivity.kt
+++ android/app/src/main/kotlin/com/amphi/notes/MainActivity.kt
@@ -1,41 +1,14 @@
 package com.amphi.notes
 
-import android.content.Intent
-import android.content.pm.ActivityInfo
-import android.content.res.Configuration
-import android.content.res.Configuration.UI_MODE_NIGHT_MASK
-import android.content.res.Configuration.UI_MODE_NIGHT_YES
-import android.net.Uri
 import android.os.Build
 import android.os.Bundle
-import android.provider.MediaStore
-import android.view.WindowInsets
-import android.view.WindowInsetsController
 import android.view.WindowManager
-import android.widget.Toast
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
 
 
 class MainActivity: FlutterActivity() {
-
-    override fun onConfigurationChanged(newConfig: Configuration) {
-        super.onConfigurationChanged(newConfig)
-        val currentNightMode = newConfig.uiMode and UI_MODE_NIGHT_MASK
-        when (currentNightMode) {
-            UI_MODE_NIGHT_YES -> {
-                if(methodChannel != null) {
-                    methodChannel!!.invokeMethod("dark_theme", "")
-                }
-            }
-            else -> {
-                if(methodChannel != null) {
-                    methodChannel!!.invokeMethod("light_theme", "")
-                }
-            }
-        }
-    }
 
     override fun onResume() {
         super.onResume()
@@ -67,82 +40,6 @@ class MainActivity: FlutterActivity() {
         methodChannel!!.setMethodCallHandler { call, result ->
             val window = this@MainActivity.window
             when (call.method) {
-                "select_image" -> {
-                    val intent = Intent(Intent.ACTION_GET_CONTENT)
-                    intent.setType("image/*")
-                    startActivityForResult(intent, REQUEST_CODE_IMAGE)
-                }
-
-                "select_video" -> {
-                    val intent = Intent(Intent.ACTION_GET_CONTENT)
-                    intent.setType("video/*")
-                    startActivityForResult(intent, REQUEST_CODE_VIDEO)
-                }
-
-                "get_storage_path" -> {
-                    result.success(storagePath)
-                }
-
-                "rotate_screen" -> {
-                    if(resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE) {
-                        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED
-                        if (Build.VERSION.SDK_INT >= 30) {
-                            window.setDecorFitsSystemWindows(true)
-                            window.insetsController?.show(WindowInsets.Type.statusBars() or WindowInsets.Type.navigationBars())
-                        }
-                        else {
-                            @Suppress("DEPRECATION")
-                            window.clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
-                        }
-                    }
-                    else {
-                        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
-                        if (Build.VERSION.SDK_INT >= 30) {
-                            window.setDecorFitsSystemWindows(false)
-                            window.insetsController?.let { controller ->
-                                controller.hide(WindowInsets.Type.statusBars() or WindowInsets.Type.navigationBars())
-                                controller.systemBarsBehavior = WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
-                            }
-                        }
-                        else {
-                            @Suppress("DEPRECATION")
-                            window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
-                        }
-                    }
-                }
-
-                "set_portrait" -> {
-                    requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED
-                    if (Build.VERSION.SDK_INT >= 30) {
-                        window.setDecorFitsSystemWindows(true)
-                        window.insetsController?.show(WindowInsets.Type.statusBars() or WindowInsets.Type.navigationBars())
-                    }
-                    else {
-                        @Suppress("DEPRECATION")
-                        window.clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
-                    }
-
-                }
-                "set_landscape" -> {
-                    requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
-                    //window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
-                    if (Build.VERSION.SDK_INT >= 30) {
-                        window.setDecorFitsSystemWindows(false)
-                        window.insetsController?.let { controller ->
-                            controller.hide(WindowInsets.Type.statusBars() or WindowInsets.Type.navigationBars())
-                            controller.systemBarsBehavior = WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
-                        }
-                    }
-                    else {
-                        @Suppress("DEPRECATION")
-                        window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
-                    }
-
-                }
-                "show_toast" -> {
-                    val message = call.argument<String>("message")
-                    Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
-                }
                 "set_navigation_bar_color" -> {
                     val color = call.argument<Long>("color")
                     val iosUi = call.argument<Boolean>("ios_like_ui")
@@ -168,63 +65,7 @@ class MainActivity: FlutterActivity() {
         }
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == REQUEST_CODE_IMAGE && resultCode == RESULT_OK) {
-            try {
-                if(data != null) {
-                    val uri = data.data
-                    if (uri != null) {
-                        methodChannel!!.invokeMethod("on_image_selected", getFilePathFromUri(uri, MediaStore.Images.Media.DATA))
-                    }
-                    else {
-                        methodChannel!!.invokeMethod("fail_image", "")
-                    }
-                }
-
-            } catch (e: Exception) {
-                methodChannel!!.invokeMethod("fail_image", "")
-                e.printStackTrace()
-            }
-        }
-        else if(requestCode == REQUEST_CODE_VIDEO && resultCode == RESULT_OK) {
-            try {
-                if(data != null) {
-                    val uri = data.data
-                    if (uri != null) {
-                        methodChannel!!.invokeMethod("on_video_selected", getFilePathFromUri(uri, MediaStore.Video.Media.DATA))
-                    }
-                    else {
-                        methodChannel!!.invokeMethod("fail_video", "")
-                    }
-                }
-            } catch (e: Exception) {
-                methodChannel!!.invokeMethod("fail_video", "")
-                e.printStackTrace()
-            }
-        }
-    }
-
-    private fun getFilePathFromUri(uri: Uri, contentType: String): String? {
-        var filePath: String? = null
-        if (uri.scheme == "content") {
-            val projection = arrayOf(contentType)
-            contentResolver.query(uri, projection, null, null, null)?.use { cursor ->
-                val columnIndex = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA)
-                if (cursor.moveToFirst()) {
-                    filePath = cursor.getString(columnIndex)
-                }
-            }
-        } else if (uri.scheme == "file") {
-            filePath = uri.path
-        }
-        return filePath
-    }
-
     companion object {
         private const val CHANNEL = "notes_method_channel"
-
-        const val REQUEST_CODE_IMAGE = 0
-        const val REQUEST_CODE_VIDEO = 1
     }
 }
--- android/app/src/main/kotlin/com/amphi/notes/NavigationBar.kt
+++ android/app/src/main/kotlin/com/amphi/notes/NavigationBar.kt
@@ -1,7 +1,7 @@
 package com.amphi.notes
 
-import android.graphics.Color
 import android.os.Build
+import android.util.Log
 import android.view.View
 import android.view.Window
 import android.view.WindowInsetsController
@@ -9,48 +9,42 @@ import android.view.WindowManager
 
 fun setNavigationBarColor(window: Window, navigationBarColor: Int, iosLikeUi: Boolean) {
     if(iosLikeUi) {
-        window.setFlags(
-            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
-            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
-        )
+        if(Build.VERSION.SDK_INT >= 30) {
+            val controller = window.insetsController
+            controller?.setSystemBarsAppearance(0, WindowInsetsController.APPEARANCE_LIGHT_NAVIGATION_BARS)
+            window.setDecorFitsSystemWindows(false)
+            window.navigationBarColor = android.graphics.Color.TRANSPARENT
+            window.statusBarColor = android.graphics.Color.TRANSPARENT
+        }
+        else {
+            @Suppress("DEPRECATION")
+            window.decorView.systemUiVisibility = (
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    )
+            window.navigationBarColor = android.graphics.Color.TRANSPARENT
+            window.statusBarColor = android.graphics.Color.TRANSPARENT
+        }
     }
     else {
-        window.clearFlags(
-            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
-        )
-        if (Build.VERSION.SDK_INT >= 30) {
-
-            window.setDecorFitsSystemWindows(false)
+        Log.d("sex", "TTAKKKKK")
+        window.clearFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
+        if(Build.VERSION.SDK_INT >= 30) {
             val controller = window.insetsController
-            val parsedColor = Color.valueOf(navigationBarColor)
-            if(parsedColor.red() + parsedColor.blue() + parsedColor.green() >= 1.5) {
-                controller?.setSystemBarsAppearance(
-                    WindowInsetsController.APPEARANCE_LIGHT_NAVIGATION_BARS,
-                    WindowInsetsController.APPEARANCE_LIGHT_NAVIGATION_BARS
-                )
-            }
-            else {
-                controller?.setSystemBarsAppearance(
-                    0,
-                    WindowInsetsController.APPEARANCE_LIGHT_NAVIGATION_BARS
-                )
-            }
-
+            window.setDecorFitsSystemWindows(true)
             window.navigationBarColor = navigationBarColor
+            window.statusBarColor = navigationBarColor
+            controller?.setSystemBarsAppearance(
+                WindowInsetsController.APPEARANCE_LIGHT_NAVIGATION_BARS,
+                WindowInsetsController.APPEARANCE_LIGHT_NAVIGATION_BARS
+            )
         }
-        else if(Build.VERSION.SDK_INT >= 26) {
-            val parsedColor = Color.valueOf(navigationBarColor)
-            if(parsedColor.red() + parsedColor.blue() + parsedColor.green() >= 1.5) {
-                @Suppress("DEPRECATION")
-                window.decorView.systemUiVisibility = window.decorView.systemUiVisibility or
-                        View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR
-            }
-            else {
-                @Suppress("DEPRECATION")
-                window.decorView.systemUiVisibility = window.decorView.systemUiVisibility and
-                        View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR.inv()
-            }
+        else {
+            @Suppress("DEPRECATION")
+            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_VISIBLE
             window.navigationBarColor = navigationBarColor
+            window.statusBarColor = navigationBarColor
         }
     }
 
