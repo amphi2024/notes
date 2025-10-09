134b7ce6bfa8e339badc42a130b37b11daaa4c28
Siar <siar019283@gmail.com>
2025-10-09 21:21:17 +0900

    refactor: move database management to singleton-based design

--- lib/utils/generate_id.dart
+++ lib/utils/generate_id.dart
@@ -1,14 +1,14 @@
 import 'dart:math';
-import 'package:sqflite/sqflite.dart';
+import 'package:notes/database/database_helper.dart';
 
-Future<String> generatedNoteId(Database db) async {
+Future<String> generatedNoteId() async {
   const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
   final random = Random();
   while(true) {
     var length = random.nextInt(5) + 15;
     final id = List.generate(length, (_) => chars[random.nextInt(chars.length)]).join();
 
-    final res = await db.rawQuery('SELECT 1 FROM notes WHERE id = ? LIMIT 1;', [id]);
+    final res = await (await databaseHelper.database).rawQuery('SELECT 1 FROM notes WHERE id = ? LIMIT 1;', [id]);
     if (res.isEmpty) {
       return id;
     }
