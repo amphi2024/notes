4bab8cb6c02d182398b0244699d01d9c35ba5d62
Siar <siar019283@gmail.com>
2025-10-09 21:33:49 +0900

    refactor: adapt logics for SQLite migration
    - remove filesystem-based note saving logic
    - adapt Note class to SQLite with isFolder flag
    - update contentToDocument() and initTitles() for SQLite data
    - adjust delete and save methods to use database

--- lib/models/note.dart
+++ lib/models/note.dart
@@ -1,29 +1,19 @@
 import 'dart:convert';
 import 'dart:core';
 import 'dart:io';
-import 'dart:ui';
-
 import 'package:amphi/extensions/color_extension.dart';
 import 'package:amphi/utils/file_name_utils.dart';
 import 'package:amphi/utils/path_utils.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_quill/flutter_quill.dart';
 import 'package:flutter_quill/quill_delta.dart';
 import 'package:notes/channels/app_web_channel.dart';
-import 'package:notes/components/note_editor/embed_block/divider/divider_block_embed.dart';
-import 'package:notes/components/note_editor/embed_block/file/file_block_embed.dart';
 import 'package:notes/components/note_editor/embed_block/image/image_block_embed.dart';
-import 'package:notes/components/note_editor/embed_block/sub_note/sub_note_block_embed.dart';
-import 'package:notes/components/note_editor/embed_block/table/note_table_block_embed.dart';
-import 'package:notes/components/note_editor/embed_block/table/table/table_data.dart';
 import 'package:notes/components/note_editor/embed_block/video/video_block_embed.dart';
-import 'package:notes/components/note_editor/embed_block/view_pager/view_pager_block_embed.dart';
-import 'package:notes/components/note_editor/embed_block/view_pager/view_pager_data.dart';
-import 'package:notes/components/note_editor/note_editing_controller.dart';
+import 'package:notes/database/database_helper.dart';
 import 'package:notes/extensions/note_extension.dart';
+import 'package:sqflite/sqflite.dart';
 
-import 'package:notes/models/content.dart';
-import 'package:notes/models/file_in_note.dart';
-import 'package:notes/models/note_embed_blocks.dart';
 
 import 'app_storage.dart';
 
@@ -37,133 +27,135 @@ class Note {
   Color? textColor;
   double? textSize;
   double? lineHeight;
-  String? font;
   String parentId = "";
   DateTime modified = DateTime.now();
   DateTime created = DateTime.now();
   DateTime? deleted;
-
-  Note({required this.id});
-
   List<Map<String, dynamic>> content = [];
 
-  Future<File> createdFileWithBase64(String base64, String fileExtension ,String directoryName) async {
-    Directory directory = Directory(PathUtils.join(appStorage.notesPath, id[0], id[1], directoryName));
-    if (!directory.existsSync()) {
-      await directory.create(recursive: true);
-    }
+  bool isFolder = false;
+  Document document = Document();
 
-    String generatedImageFilename = FilenameUtils.generatedFileName(fileExtension, directory.path);
-
-    File file = File(PathUtils.join(directory.path, generatedImageFilename));
-    await file.writeAsBytes(base64Decode(base64));
-
-    return file;
+  Note({
+    required this.id,
+    this.title = "",
+    this.subtitle = "",
+    this.longSubtitle = "",
+    this.thumbnailImageFilename,
+    this.backgroundColor,
+    this.textColor,
+    this.textSize,
+    this.lineHeight,
+    this.parentId = "",
+    DateTime? modified,
+    DateTime? created,
+    this.deleted,
+    this.content = const []
+  }) {
+    document = contentToDocument();
   }
 
-  Future<File> createdFile(String originalPath, String directoryName) async {
-    Directory directory = Directory(PathUtils.join(appStorage.notesPath, id[0], id[1], directoryName));
-    if (!directory.existsSync()) {
-      await directory.create(recursive: true);
+  Note.fromMap(Map<String, dynamic> data) : id = data["id"] {
+    if(data["content"] is String) {
+      var decoded = jsonDecode(data["content"]);
+      if(decoded is List<dynamic>) {
+        content = decoded.map((e) => e as Map<String, dynamic>).toList();
+      }
     }
 
-    String generatedImageFilename = FilenameUtils.generatedFileName(PathUtils.extension(originalPath), directory.path);
+    title = data["title"] ?? "";
+    subtitle = data["subtitle"] ?? "";
+    isFolder = data["is_folder"] == 1;
+    created = DateTime.fromMillisecondsSinceEpoch(data["created"]).toLocal();
+    modified = DateTime.fromMillisecondsSinceEpoch(data["modified"]).toLocal();
+    parentId = data["parent_id"] ?? "";
 
-    File originalFile = File(originalPath);
-    File file = File(PathUtils.join(directory.path, generatedImageFilename));
-    await originalFile.copy(file.path);
-
-    return file;
+    if(!isFolder) {
+      initTitles();
+      document = contentToDocument();
+    }
   }
-
-  Future<File> createdImageFile(String originalPath) async => createdFile(originalPath, "images");
-
-  Future<File> createdVideoFile(String originalPath) async => createdFile(originalPath, "videos");
-
-  Future<File> createdImageFileWithBase64(String base64, String fileExtension) async => createdFileWithBase64(base64, fileExtension , "images");
-
-  Future<File> createdVideoFileWithBase64(String base64, String fileExtension) async => createdFileWithBase64(base64, fileExtension , "videos");
 
   static Note subNote(Note parent) {
     return Note(id: "");
   }
 
-  Document toDocument() {
+  Document contentToDocument() {
     Delta delta = Delta();
-    // for (Content content in content) {
-    //   switch (content.type) {
-    //     case "img":
-    //       BlockEmbed blockEmbed = BlockEmbed.custom(ImageBlockEmbed(content.value));
-    //       delta.insert(blockEmbed.toJson());
-    //       break;
-    //     case "video":
-    //       BlockEmbed blockEmbed = BlockEmbed.custom(VideoBlockEmbed(content.value));
-    //       delta.insert(blockEmbed.toJson());
-    //       break;
-    //     case "table":
-    //       String tableKey = noteEmbedBlocks.generatedTableKey();
-    //       noteEmbedBlocks.tables[tableKey] = TableData.fromContent(content);
-    //       BlockEmbed blockEmbed = BlockEmbed.custom(NoteTableBlockEmbed(tableKey));
-    //       delta.insert(blockEmbed.toJson());
-    //       break;
-    //     case "note":
-    //       Map<String, dynamic> subNoteData = content.value;
-    //
-    //       Note subNote = Note.subNote(this);
-    //       subNote.title = subNoteData["title"] ?? "";
-    //       for (dynamic data in subNoteData["contents"] ?? []) {
-    //         Map<String, dynamic> map = data;
-    //         subNote.content.add(Content(map));
-    //       }
-    //
-    //       String subNoteKey = noteEmbedBlocks.generatedSubNoteKey();
-    //       noteEmbedBlocks.subNotes[subNoteKey] = NoteEditingController(note: subNote, readOnly: true);
-    //       BlockEmbed blockEmbed = BlockEmbed.custom(SubNoteBlockEmbed(subNoteKey));
-    //       delta.insert(blockEmbed.toJson());
-    //
-    //       break;
-    //     case "divider":
-    //       String dividerKey = noteEmbedBlocks.generatedDividerKey();
-    //       if (content.style != null) {
-    //         noteEmbedBlocks.dividers[dividerKey] = Color(content.style!["color"] ?? 0);
-    //       }
-    //       BlockEmbed divider = BlockEmbed.custom(DividerBlockEmbed(dividerKey));
-    //       delta.insert(divider.toJson());
-    //
-    //       break;
-    //     case "view-pager":
-    //       String viewPagerKey = noteEmbedBlocks.generatedViewPagerKey();
-    //       noteEmbedBlocks.viewPagers[viewPagerKey] = ViewPagerData.fromContent(this, content);
-    //       BlockEmbed blockEmbed = BlockEmbed.custom(ViewPagerBlockEmbed(viewPagerKey));
-    //       delta.insert(blockEmbed.toJson());
-    //       break;
-    //     case "file":
-    //       var key = noteEmbedBlocks.generatedFileKey();
-    //       noteEmbedBlocks.files[key] = FileInNote.fromContent(content);
-    //       BlockEmbed blockEmbed = BlockEmbed.custom(FileBlockEmbed(key));
-    //       delta.insert(blockEmbed.toJson());
-    //       break;
-    //     case "text":
-    //       if (content.value is String) {
-    //         // if(!content.value.endsWith("\n")) {
-    //         //   content.value = content.value + "\n";
-    //         // }
-    //         // String text = content.value;
-    //
-    //         delta.insert(content.value, content.style);
-    //         break;
-    //       }
-    //
-    //
-    //     default:
-    //       // if(!content.value.toString().endsWith("\n")) {
-    //       //   content.value = content.value.toString() + "\n";
-    //       // }
-    //       delta.insert(content.value.toString());
-    //
-    //       break;
-    //   }
-    // }
+    for (var item in content) {
+      switch (item["type"]) {
+        case "img":
+          BlockEmbed blockEmbed = BlockEmbed.custom(ImageBlockEmbed(item["value"]));
+          delta.insert(blockEmbed.toJson());
+          break;
+        case "video":
+          BlockEmbed blockEmbed = BlockEmbed.custom(VideoBlockEmbed(item["value"]));
+          delta.insert(blockEmbed.toJson());
+          break;
+        case "table":
+          // String tableKey = noteEmbedBlocks.generatedTableKey();
+          // noteEmbedBlocks.tables[tableKey] = TableData.fromContent(content);
+          // BlockEmbed blockEmbed = BlockEmbed.custom(NoteTableBlockEmbed(tableKey));
+          // delta.insert(blockEmbed.toJson());
+          // break;
+        case "note":
+          // Map<String, dynamic> subNoteData = item["value"];
+          //
+          // Note subNote = Note.subNote(this);
+          // subNote.title = subNoteData["title"] ?? "";
+          // for (dynamic data in subNoteData["contents"] ?? []) {
+          //   Map<String, dynamic> map = data;
+          //   subNote.content.add(Content(map));
+          // }
+          //
+          // String subNoteKey = noteEmbedBlocks.generatedSubNoteKey();
+          // noteEmbedBlocks.subNotes[subNoteKey] = NoteEditingController(note: subNote, readOnly: true);
+          // BlockEmbed blockEmbed = BlockEmbed.custom(SubNoteBlockEmbed(subNoteKey));
+          // delta.insert(blockEmbed.toJson());
+          //
+          // break;
+        // case "divider":
+        //   String dividerKey = noteEmbedBlocks.generatedDividerKey();
+        //   if (content.style != null) {
+        //     noteEmbedBlocks.dividers[dividerKey] = Color(content.style!["color"] ?? 0);
+        //   }
+        //   BlockEmbed divider = BlockEmbed.custom(DividerBlockEmbed(dividerKey));
+        //   delta.insert(divider.toJson());
+        //
+        //   break;
+        // case "view-pager":
+        //   String viewPagerKey = noteEmbedBlocks.generatedViewPagerKey();
+        //   noteEmbedBlocks.viewPagers[viewPagerKey] = ViewPagerData.fromContent(this, content);
+        //   BlockEmbed blockEmbed = BlockEmbed.custom(ViewPagerBlockEmbed(viewPagerKey));
+        //   delta.insert(blockEmbed.toJson());
+        //   break;
+        // case "file":
+        //   var key = noteEmbedBlocks.generatedFileKey();
+        //   noteEmbedBlocks.files[key] = FileInNote.fromContent(content);
+        //   BlockEmbed blockEmbed = BlockEmbed.custom(FileBlockEmbed(key));
+        //   delta.insert(blockEmbed.toJson());
+        //   break;
+        case "text":
+          if (item["value"] is String) {
+            // if(!item["value"].endsWith("\n")) {
+            //   item["value"] = item["value"] + "\n";
+            // }
+            // String text = item["value"];
+
+            delta.insert(item["value"], item["style"]);
+            break;
+          }
+
+
+        default:
+          // if(!item["value"].toString().endsWith("\n")) {
+          //   item["value"] = item["value"].toString() + "\n";
+          // }
+          delta.insert(item["value"].toString());
+
+          break;
+      }
+    }
 
     if (delta.isEmpty) {
       delta.insert("\n");
@@ -177,9 +169,9 @@ class Note {
   }
 
   Future<void> delete({bool upload = true}) async {
-    var images = Directory(PathUtils.join(appStorage.notesPath, id[0], id[1], "images"));
-    var videos = Directory(PathUtils.join(appStorage.notesPath, id[0], id[1], "videos"));
-    var audio = Directory(PathUtils.join(appStorage.notesPath, id[0], id[1], "audio"));
+    var images = Directory(PathUtils.join(appStorage.attachmentsPath, id[0], id[1], "images"));
+    var videos = Directory(PathUtils.join(appStorage.attachmentsPath, id[0], id[1], "videos"));
+    var audio = Directory(PathUtils.join(appStorage.attachmentsPath, id[0], id[1], "audio"));
     if(await images.exists()) {
       images.delete(recursive: true);
     }
@@ -307,7 +299,33 @@ class Note {
   }
 
   Future<void> save({bool upload = true}) async {
+    final database = await databaseHelper.database;
+    await database.rawInsert("""
+    INSERT INTO notes (
+                    id, content, created, modified, deleted, is_folder, parent_id,
+                    line_height, text_size, text_color, background_color, background,
+                    title, subtitle
+                ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+                ON CONFLICT(id) DO UPDATE SET
+                  content = excluded.content,
+                  modified = excluded.modified,
+                  deleted = excluded.deleted,
+                  is_folder = excluded.is_folder,
+                  parent_id = excluded.parent_id,
+                  line_height = excluded.line_height,
+                  text_size = excluded.text_size,
+                  text_color = excluded.text_color,
+                  background_color = excluded.background_color,
+                  background = excluded.background,
+                  title = excluded.title,
+                  subtitle = excluded.subtitle;
+    """, [
+      id, isFolder? null : jsonEncode(content), created.toUtc().millisecondsSinceEpoch, modified.toUtc().millisecondsSinceEpoch, deleted?.toUtc().millisecondsSinceEpoch, isFolder ? 1 : 0, parentId.isEmpty ? null : parentId, lineHeight?.toInt(), textSize?.toInt(), textColor?.value, backgroundColor?.value,  null, isFolder ? title : null, null
+    ]);
 
+    if(upload) {
+      appWebChannel.uploadNote(this);
+    }
   }
 
   Future<void> saveDraft() async {
@@ -320,57 +338,76 @@ class Note {
     longSubtitle = "";
     thumbnailImageFilename = null;
 
-    // for (Content content in content) {
-    //   if (content.type == "text" && content.value is String) {
-    //     List<String> textLines = content.value.split("\n");
-    //     if (textLines.length > 1) {
-    //       for (String line in textLines) {
-    //         if (line.trim().isNotEmpty) {
-    //           if (title.isEmpty) {
-    //             title = line;
-    //           } else {
-    //             if (subtitle.isEmpty) {
-    //               subtitle = line;
-    //             }
-    //             if(longSubtitle.length < 500) {
-    //               longSubtitle += line + "\n";
-    //             }
-    //           }
-    //         }
-    //       }
-    //     } else {
-    //       if (content.value.trim().isNotEmpty) {
-    //         if (title.isEmpty) {
-    //           title = content.value;
-    //         } else {
-    //           if (subtitle.isEmpty) {
-    //           subtitle = content.value;
-    //           }
-    //           if(longSubtitle.length < 500) {
-    //             longSubtitle += content.value + "\n";
-    //           }
-    //         }
-    //       }
-    //     }
-    //   }
-    //   if (thumbnailImageFilename == null) {
-    //     if (content.type == "img") {
-    //       thumbnailImageFilename = content.value;
-    //     }
-    //   }
-    //   if (thumbnailImageFilename != null && title.isNotEmpty && subtitle.isNotEmpty && longSubtitle.length > 500) {
-    //     break;
-    //   }
-    // }
+    for (var item in content) {
+      if (item["type"] == "text" && item["value"] is String) {
+        List<String> textLines = item["value"].split("\n");
+        if (textLines.length > 1) {
+          for (String line in textLines) {
+            if (line.trim().isNotEmpty) {
+              if (title.isEmpty) {
+                title = line;
+              } else {
+                if (subtitle.isEmpty) {
+                  subtitle = line;
+                }
+                if(longSubtitle.length < 500) {
+                  longSubtitle += line + "\n";
+                }
+              }
+            }
+          }
+        } else {
+          if (item["value"].trim().isNotEmpty) {
+            if (title.isEmpty) {
+              title = item["value"];
+            } else {
+              if (subtitle.isEmpty) {
+              subtitle = item["value"];
+              }
+              if(longSubtitle.length < 500) {
+                longSubtitle += item["value"] + "\n";
+              }
+            }
+          }
+        }
+      }
+      if (thumbnailImageFilename == null) {
+        if (item["type"] == "img") {
+          thumbnailImageFilename = item["value"];
+        }
+      }
+      if (thumbnailImageFilename != null && title.isNotEmpty && subtitle.isNotEmpty && longSubtitle.length > 500) {
+        break;
+      }
+    }
 
   }
 
-  Color? textColorByTheme(bool darkMode) {
-    return darkMode ? textColor?.inverted() : textColor;
+  Map<String, dynamic> toMap() {
+    return {
+      "id": id,
+      "title": isFolder ? title : null,
+      "subtitle": isFolder ? null : subtitle,
+      "background_color": backgroundColor?.value,
+      "text_color": textColor?.value,
+      "text_size": textSize?.toInt(),
+      "line_height": lineHeight?.toInt(),
+      "parent_id": parentId.isEmpty ? null : parentId,
+      "modified": modified.toUtc().millisecondsSinceEpoch,
+      "created": created.toUtc().millisecondsSinceEpoch,
+      "deleted": deleted?.toUtc().millisecondsSinceEpoch,
+      "content": jsonEncode(content),
+      "is_folder": isFolder ? 1 : 0,
+      "background": null
+    };
   }
 
-  Color? backgroundColorByTheme(bool darkMode) {
-    return darkMode ? backgroundColor?.inverted() : backgroundColor;
+  Color? textColorByTheme(BuildContext context) {
+    return Theme.of(context).brightness == Brightness.dark ? textColor?.inverted() : textColor;
+  }
+
+  Color backgroundColorByTheme(BuildContext context) {
+    return (Theme.of(context).brightness == Brightness.dark ? backgroundColor?.inverted() : backgroundColor) ?? Theme.of(context).cardColor;
   }
 
 }
