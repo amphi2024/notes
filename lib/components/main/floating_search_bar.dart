29c4211532a3d466330e95fe8ecdaabb8676289a
Siar <siar019283@gmail.com>
2025-10-09 21:04:55 +0900

    refactor: switch state management to Riverpod

--- lib/components/main/floating_search_bar.dart
+++ lib/components/main/floating_search_bar.dart
@@ -1,48 +1,65 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../../providers/providers.dart';
+import '../../providers/selected_notes_provider.dart';
 
-import '../../main.dart';
-
-class FloatingSearchBar extends StatelessWidget {
-
-  final bool showing;
-  final FocusNode focusNode;
-  final TextEditingController textEditingController;
-  const FloatingSearchBar({super.key, required this.showing, required this.focusNode, required this.textEditingController});
+class FloatingSearchBar extends ConsumerWidget {
+  const FloatingSearchBar(
+      {super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return  AnimatedPositioned(
-      duration: Duration(milliseconds: showing ? 1000 : 1250),
+  Widget build(BuildContext context, WidgetRef ref) {
+    final selectingNotes = ref.watch(selectedNotesProvider) != null;
+    final buttonRotated = ref.watch(floatingButtonStateProvider);
+    var bottomPadding = MediaQuery.of(context).padding.bottom;
+    if(bottomPadding == 0) {
+      bottomPadding = 15;
+    }
+
+    return AnimatedPositioned(
+      duration: Duration(milliseconds: !selectingNotes && buttonRotated ? 1000 : 1250),
       curve: Curves.easeOutQuint,
-      left: showing ? 20 : -235,
-      bottom: bottomPaddingIfAndroid3Button(context) + 120,
+      left: !selectingNotes && buttonRotated ? 20 : -235,
+      bottom: bottomPadding + 105,
       child: Container(
         width: 200,
         height: 45,
         decoration: BoxDecoration(
             boxShadow: [
               BoxShadow(
-                color: Theme.of(context).shadowColor,
+                color: Theme
+                    .of(context)
+                    .shadowColor,
                 spreadRadius: 3,
                 blurRadius: 5,
                 offset: const Offset(0, 3),
               ),
             ],
-            color: Theme.of(context).floatingActionButtonTheme.backgroundColor,
+            color: Theme
+                .of(context)
+                .floatingActionButtonTheme
+                .backgroundColor,
             borderRadius: BorderRadius.circular(15)
         ),
         child: Center(
           child: Padding(
             padding: const EdgeInsets.only(right: 10.0),
             child: TextField(
-              focusNode: focusNode,
-              controller: textEditingController,
+              onChanged: (text) {
+                ref.read(searchKeywordProvider.notifier).setKeyword(text);
+              },
+              onTapOutside: (event) {
+                ref.read(searchKeywordProvider.notifier).endSearch();
+              },
               decoration: InputDecoration(
                   border: InputBorder.none,
                   focusedBorder: InputBorder.none,
                   prefixIcon: Icon(
                     Icons.search,
-                    color: Theme.of(context).colorScheme.primary,
+                    color: Theme
+                        .of(context)
+                        .colorScheme
+                        .primary,
                   )
               ),
             ),
