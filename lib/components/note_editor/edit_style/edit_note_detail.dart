9ff29709e08347c0dcdc7a36a5c936aa3f1b14e1
siar1234 <siar019283@gmail.com>
2025-11-05 15:41:01 +0900

    fix: restore responsive UI for different screen sizes
    - Considered replacing Flutter with QT ON desktop, but decided against it due to limited benefits and development time

--- lib/components/note_editor/edit_style/edit_note_detail.dart
+++ lib/components/note_editor/edit_style/edit_note_detail.dart
@@ -20,14 +20,6 @@ class EditNoteDetail extends ConsumerStatefulWidget {
 }
 
 class _EditNoteDetailState extends ConsumerState<EditNoteDetail> {
-  late final TextEditingController lineHeightController = TextEditingController(
-      text: ref.watch(editingNoteProvider).note.lineHeight?.toString() ?? "1.0");
-
-  @override
-  void dispose() {
-    lineHeightController.dispose();
-    super.dispose();
-  }
 
   void addNoteTextColor(Color color) {
     appColors.noteTextColors.add(color);
@@ -40,137 +32,122 @@ class _EditNoteDetailState extends ConsumerState<EditNoteDetail> {
   }
 
   @override
-  void initState() {
-    lineHeightController.addListener(() {
-      var lineHeight = double.tryParse(lineHeightController.text);
-      if (lineHeight == 1.0) {
-        ref.read(editingNoteProvider.notifier).setLineHeight(null);
-      } else {
-        ref.read(editingNoteProvider.notifier).setLineHeight(lineHeight);
-      }
-    });
-    super.initState();
-  }
-
-  @override
   Widget build(BuildContext context) {
     final themeData = Theme.of(context);
     final note = ref.watch(editingNoteProvider).note;
     return Container(
       width: 250,
       height: App.isDesktop() ? 200 : 400,
-      child: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(10),
-          child: Column(
-            children: [
-              Row(
-                children: [
-                  Text(AppLocalizations.of(context)
-                      .get("@note_background_color")),
-                  IconButton(
-                      icon: Icon(Icons.circle,
-                          color: note.backgroundColorByTheme(context)),
-                      onPressed: () {
-                        showAdaptiveColorPicker(
-                            context: context,
-                            color: note.backgroundColorByTheme(context),
-                            onAddColor: (color) {
-                              appColors.noteBackgroundColors.add(color);
-                              appColors.save();
-                            },
-                            onColorChanged: (value) {
-                              var color = value;
-                              if (Theme.of(context).brightness ==
-                                  Brightness.dark) {
-                                color = value.inverted();
-                              }
-                              ref
-                                  .read(editingNoteProvider.notifier)
-                                  .setBackgroundColor(color);
-                            },
-                            colors: appColors.noteTextColors,
-                            onRemoveColor: (color) {
-                              appColors.noteBackgroundColors.remove(color);
-                              appColors.save();
-                            },
-                            defaultColor: themeData.cardColor,
-                            onDefaultColorTap: (value) {
-                              ref
-                                  .read(editingNoteProvider.notifier)
-                                  .setBackgroundColor(null);
-                            });
-                      })
-                ],
-              ),
-              Row(
-                children: [
-                  Text(AppLocalizations.of(context).get("@note_text_color")),
-                  IconButton(
-                      icon: Icon(Icons.circle,
+      child: Padding(
+        padding: const EdgeInsets.all(10),
+        child: ListView(
+          children: [
+            Row(
+              children: [
+                Text(AppLocalizations.of(context)
+                    .get("@note_background_color")),
+                IconButton(
+                    icon: Icon(Icons.circle,
+                        color: note.backgroundColorByTheme(context)),
+                    onPressed: () {
+                      showAdaptiveColorPicker(
+                          context: context,
+                          color: note.backgroundColorByTheme(context),
+                          onAddColor: (color) {
+                            appColors.noteBackgroundColors.add(color);
+                            appColors.save();
+                          },
+                          onColorChanged: (value) {
+                            var color = value;
+                            if (Theme.of(context).brightness ==
+                                Brightness.dark) {
+                              color = value.inverted();
+                            }
+                            ref
+                                .read(editingNoteProvider.notifier)
+                                .setBackgroundColor(color);
+                          },
+                          colors: appColors.noteTextColors,
+                          onRemoveColor: (color) {
+                            appColors.noteBackgroundColors.remove(color);
+                            appColors.save();
+                          },
+                          defaultColor: themeData.cardColor,
+                          onDefaultColorTap: (value) {
+                            ref
+                                .read(editingNoteProvider.notifier)
+                                .setBackgroundColor(null);
+                          });
+                    })
+              ],
+            ),
+            Row(
+              children: [
+                Text(AppLocalizations.of(context).get("@note_text_color")),
+                IconButton(
+                    icon: Icon(Icons.circle,
+                        color: note.textColorByTheme(context) ??
+                            themeData.textTheme.bodyMedium!.color),
+                    onPressed: () {
+                      showAdaptiveColorPicker(
+                          context: context,
                           color: note.textColorByTheme(context) ??
-                              themeData.textTheme.bodyMedium!.color),
-                      onPressed: () {
-                        showAdaptiveColorPicker(
-                            context: context,
-                            color: note.textColorByTheme(context) ??
-                                themeData.textTheme.bodyMedium!.color!,
-                            onAddColor: addNoteTextColor,
-                            onColorChanged: (value) {
-                              var color = value;
-                              if (Theme.of(context).brightness ==
-                                  Brightness.dark) {
-                                color = value.inverted();
-                              }
-                              ref
-                                  .read(editingNoteProvider.notifier)
-                                  .setTextColor(color);
-                            },
-                            colors: appColors.noteTextColors,
-                            onRemoveColor: removeNoteTextColor,
-                            defaultColor:
-                                Theme.of(context).textTheme.bodyMedium!.color,
-                            onDefaultColorTap: (color) {
-                              ref
-                                  .read(editingNoteProvider.notifier)
-                                  .setTextColor(null);
-                            });
-                      })
-                ],
-              ),
-              Row(
-                children: [
-                  Text(AppLocalizations.of(context).get("@note_text_size")),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 8.0),
-                    child: EditNoteTextSize(
-                        value: note.textSize ?? 15,
-                        controller: widget.controller,
-                        onChange: (size) {
-                          ref
-                              .read(editingNoteProvider.notifier)
-                              .setTextSize(size);
-                        }),
-                  )
-                ],
-              ),
-              Row(
-                children: [
-                  Text(AppLocalizations.of(context).get("@note_line_height")),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 8.0),
-                    child: _EditNoteLineHeight(
-                        value: note.lineHeight ?? 1,
-                        onChange: (value) {
-                          ref
-                              .read(editingNoteProvider.notifier)
-                              .setLineHeight(value);
-                        }),
-                  )
-                ],
-              ),
-            ],
-          ),
+                              themeData.textTheme.bodyMedium!.color!,
+                          onAddColor: addNoteTextColor,
+                          onColorChanged: (value) {
+                            var color = value;
+                            if (Theme.of(context).brightness ==
+                                Brightness.dark) {
+                              color = value.inverted();
+                            }
+                            ref
+                                .read(editingNoteProvider.notifier)
+                                .setTextColor(color);
+                          },
+                          colors: appColors.noteTextColors,
+                          onRemoveColor: removeNoteTextColor,
+                          defaultColor:
+                              Theme.of(context).textTheme.bodyMedium!.color,
+                          onDefaultColorTap: (color) {
+                            ref
+                                .read(editingNoteProvider.notifier)
+                                .setTextColor(null);
+                          });
+                    })
+              ],
+            ),
+            Row(
+              children: [
+                Text(AppLocalizations.of(context).get("@note_text_size")),
+                Padding(
+                  padding: const EdgeInsets.only(left: 8.0),
+                  child: EditNoteTextSize(
+                      value: note.textSize ?? 15,
+                      controller: widget.controller,
+                      onChange: (size) {
+                        ref
+                            .read(editingNoteProvider.notifier)
+                            .setTextSize(size);
+                      }),
+                )
+              ],
+            ),
+            Row(
+              children: [
+                Text(AppLocalizations.of(context).get("@note_line_height")),
+                Padding(
+                  padding: const EdgeInsets.only(left: 8.0),
+                  child: _EditNoteLineHeight(
+                      value: note.lineHeight ?? 1,
+                      onChange: (value) {
+                        ref
+                            .read(editingNoteProvider.notifier)
+                            .setLineHeight(value);
+                      }),
+                )
+              ],
+            ),
+          ],
         ),
       ),
     );
@@ -195,7 +172,7 @@ class _EditNoteLineHeightState extends State<_EditNoteLineHeight> {
 
   @override
   Widget build(BuildContext context) {
-    if (App.isDesktop()) {
+    if (App.isDesktop() || App.isWideScreen(context)) {
       return DropdownButton<double>(
           value: widget.value,
           items: list
--- lib/components/note_editor/edit_style/edit_note_text_size.dart
+++ lib/components/note_editor/edit_style/edit_note_text_size.dart
@@ -1,8 +1,12 @@
+import 'package:amphi/models/app.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_quill/flutter_quill.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:notes/components/note_editor/editor_extension.dart';
+import 'package:notes/providers/editing_note_provider.dart';
 
-class EditNoteTextSize extends StatefulWidget {
+class EditNoteTextSize extends StatelessWidget {
 
   final QuillController controller;
   final void Function(double) onChange;
@@ -10,14 +14,75 @@ class EditNoteTextSize extends StatefulWidget {
   const EditNoteTextSize({super.key, required this.controller, required this.onChange, required this.value});
 
   @override
-  State<EditNoteTextSize> createState() => _EditNoteTextSizeState();
+  Widget build(BuildContext context) {
+    if (App.isDesktop() || App.isWideScreen(context)) {
+      return _Wide(
+        controller: controller,
+        onChange: onChange,
+        value: value,
+      );
+    }
+    return _Mobile(
+      controller: controller,
+      onChange: onChange,
+      value: value,
+    );
+  }
 }
 
-class _EditNoteTextSizeState extends State<EditNoteTextSize> {
+class _Wide extends ConsumerStatefulWidget {
+
+  final QuillController controller;
+  final void Function(double) onChange;
+  final double value;
+
+  const _Wide({required this.controller, required this.onChange, required this.value});
+
+  @override
+  ConsumerState<_Wide> createState() => _WideState();
+}
+
+class _WideState extends ConsumerState<_Wide> {
+
+  final List<double> list = List.generate(90, (size) => size + 5.toDouble());
+
+  @override
+  Widget build(BuildContext context) {
+
+    final defaultSize = ref.watch(editingNoteProvider).note.textSize ?? 15;
+
+    return DropdownButton<double>(
+        style: Theme
+            .of(context)
+            .textTheme
+            .bodyMedium,
+        value: widget.value,
+        items: list.map((item) => DropdownMenuItem<double>(value: item, child: Text(defaultSize == item ? "Default": item.toInt().toString()))).toList(),
+    onChanged: (item) {
+    widget.onChange(item ?? 15);
+    });
+  }
+}
+
+
+class _Mobile extends StatefulWidget {
+
+  final QuillController controller;
+  final void Function(double) onChange;
+  final double value;
+
+  const _Mobile({required this.controller, required this.onChange, required this.value});
+
+  @override
+  State<_Mobile> createState() => _MobileState();
+}
+
+class _MobileState extends State<_Mobile> {
 
   late final FixedExtentScrollController fixedExtentScrollController =
   FixedExtentScrollController(
       initialItem: widget.controller.getFontSizeIndex(list));
+  final List<double> list = List.generate(90, (size) => size + 5.toDouble());
 
 
   @override
@@ -26,19 +91,18 @@ class _EditNoteTextSizeState extends State<EditNoteTextSize> {
     super.dispose();
   }
 
-  final List<double> list = List.generate(90, (size) => size + 5.toDouble());
   @override
   Widget build(BuildContext context) {
-      return SizedBox(
-        width: 150,
-        height: 200,
-        child: CupertinoPicker(
-            scrollController: fixedExtentScrollController,
-            itemExtent: 30,
-            onSelectedItemChanged: (i) async {
-              widget.onChange(list[i]);
-            },
-            children: list.map((item) => Text(item.toInt().toString())).toList() ),
-      );
+    return SizedBox(
+      width: 150,
+      height: 200,
+      child: CupertinoPicker(
+          scrollController: fixedExtentScrollController,
+          itemExtent: 30,
+          onSelectedItemChanged: (i) async {
+            widget.onChange(list[i]);
+          },
+          children: list.map((item) => Text(item.toInt().toString())).toList()),
+    );
   }
 }
