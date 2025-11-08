3056a4487a604fa1c9252bfc1fcaef5247f9bfb0
siar1234 <siar019283@gmail.com>
2025-11-08 13:24:45 +0900

    refactor: update note import from file for SQLite and Riverpod migration, and add Markdown support

--- lib/components/note_editor/toolbar/note_editor_import_button.dart
+++ lib/components/note_editor/toolbar/note_editor_import_button.dart
@@ -1,123 +1,34 @@
-
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_quill/flutter_quill.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:notes/icons/icons.dart';
 
-class NoteEditorImportButton extends StatelessWidget {
+import '../../../models/note.dart';
+import '../../../utils/note_import_utils.dart';
 
+class NoteEditorImportButton extends ConsumerWidget {
+  final Note note;
   final QuillController controller;
-  const NoteEditorImportButton({super.key, required this.controller});
 
-  void importFromNote() async {
-    // var selectedFiles = await FilePicker.platform.pickFiles(allo);
-    // var file = selectedFiles?.files.firstOrNull;
-    // if(file == null) {
-    //   return;
-    // }
-    // var map = file()
-
-    // if(selectedFiles != null) {
-    //   for(var file in selectedFiles.files) {
-    //     String fileContent = await file.xFile.readAsString();
-    //     Note note = Note.fromFileContent(fileContent: fileContent, originalModified: DateTime.now(), filePath: file.path ?? "");
-    //     var directory = Directory(file.xFile.path.split(".").first);
-    //       for(Content content in note.contents) {
-    //         if(content.type == "img") {
-    //           try {
-    //             String value = content.value;
-    //             if(value.startsWith("!BASE64")) {
-    //               var split = value.split(";");
-    //               String imageExtension = split[1];
-    //               String base64 = split[2];
-    //               var file = await noteEditingController.note.createdImageFileWithBase64(base64, imageExtension);
-    //               var image = Content(
-    //                   value: PathUtils.basename(file.path),
-    //                   type: "img"
-    //               );
-    //               noteEditingController.note.contents.add(image);
-    //             }
-    //             else {
-    //               var file = await noteEditingController.note.createdImageFile(PathUtils.join(directory.path, value));
-    //               var image = Content(
-    //                   value: PathUtils.basename(file.path),
-    //                   type: "img"
-    //               );
-    //               noteEditingController.note.contents.add(image);
-    //             }
-    //           }
-    //           catch(e) {
-    //             noteEditingController.note.contents.add(Content(
-    //               value: "{IMAGE}\n",
-    //               type: "text"
-    //             ));
-    //           }
-    //
-    //         }
-    //         else if(content.type == "video") {
-    //           try {
-    //           var file = await noteEditingController.note.createdVideoFile(PathUtils.join(directory.path, content.value));
-    //           var video = Content(
-    //               value: PathUtils.basename(file.path),
-    //               type: "video"
-    //           );
-    //           noteEditingController.note.contents.add(video);
-    //           }
-    //           catch(e) {
-    //             noteEditingController.note.contents.add(Content(
-    //                 value: "{VIDEO}\n",
-    //                 type: "text"
-    //             ));
-    //           }
-    //         }
-    //         else {
-    //           noteEditingController.note.contents.add(content);
-    //         }
-    //       }
-    //     }
-    //     noteEditingController.document = noteEditingController.note.toDocument();
-    //
-    // }
-
-  }
-
-  // void importFromHTML() async {
-  //   Note note = noteEditingController.note;
-  //   String? selectedPath = await FilePicker.platform.saveFile(fileName: "${note.title}.html");
-  //
-  //
-  // }
-  //
-  // void importFromMarkdown() async {
-  //   Note note = noteEditingController.note;
-  //   String? selectedPath = await FilePicker.platform.saveFile(fileName: "${note.title}.md");
-  //
-  // }
-
-  // void importFromWord() async {
-  //   Note note = noteEditingController.note;
-  //   String? selectedPath = await FilePicker.platform.saveFile(fileName: "${note.title}.html");
-  //
-  //   if (selectedPath != null) {
-  //
-  //   }
-  // }
+  const NoteEditorImportButton({super.key, required this.controller, required this.note});
 
   @override
-  Widget build(BuildContext context) {
-    //var localizations = AppLocalizations.of(context);
+  Widget build(BuildContext context, WidgetRef ref) {
     return IconButton(
-      icon: Icon(AppIcons.import, size: 20),
-      onPressed: () {
-        importFromNote();
-        // showMenuByRelative(context: context, items: [
-        //   PopupMenuItem(child: Text(localizations.get("@note_export_label_note")), onTap: importFromNote),
-        //   // PopupMenuItem(child: Text(localizations.get("@note_export_label_html")), onTap: importFromHTML),
-        //   // PopupMenuItem(child: Text(localizations.get("@note_export_label_markdown")), onTap: importFromMarkdown),
-        //   //PopupMenuItem(child: Text(localizations.get("@note_export_label_word")), onTap: importFromWord),
-        //   // PopupMenuItem(child: Text(localizations.get("@note_export_label_pdf")), onTap: exportAsPDF),
-        // ]);
-      },
-    );
+        icon: Icon(AppIcons.import, size: 20),
+        onPressed: () async {
+          final selectedFiles = await FilePicker.platform.pickFiles(allowMultiple: false, type: FileType.custom, allowedExtensions: ["note", "md"]);
+          final file = selectedFiles?.files.firstOrNull;
+          if (file != null) {
+            final fileContent = await file.xFile.readAsString();
+            if (file.extension == "note") {
+              controller.importNote(noteId: note.id, fileContent: fileContent, ref: ref);
+            }
+            else if (file.extension == "md") {
+              controller.importMarkdown(fileContent);
+            }
+          }
+        });
   }
 }
---
+++ lib/utils/note_import_utils.dart
@@ -0,0 +1,107 @@
+import 'dart:convert';
+
+import 'package:flutter_quill/flutter_quill.dart';
+import 'package:flutter_quill/quill_delta.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:markdown/markdown.dart' as md;
+import 'package:markdown_quill/markdown_quill.dart';
+import 'package:notes/components/note_editor/embed_block/audio/audio_block_embed.dart';
+import 'package:notes/components/note_editor/embed_block/video/video_block_embed.dart';
+import 'package:notes/providers/tables_provider.dart';
+import 'package:notes/utils/select_file_utils.dart';
+
+import '../components/note_editor/embed_block/divider/divider_block_embed.dart';
+import '../components/note_editor/embed_block/image/image_block_embed.dart';
+import '../components/note_editor/embed_block/table/note_table_block_embed.dart';
+import '../models/table_data.dart';
+
+extension ImportExtension on QuillController {
+  void importNote({required String noteId, required String fileContent, required WidgetRef ref}) async {
+    try {
+      final delta = Delta();
+      final data = jsonDecode(fileContent);
+      final content = data["content"] ?? ["contents"];
+      if (content is List<dynamic>) {
+        for (var item in content) {
+          final value = item["value"];
+
+          switch (item["type"]) {
+            case "img":
+              if (value is String) {
+                var split = value.split(";");
+                if (split.length > 2) {
+                  // !BASE64;jpg;abc.....
+                  final file = await createdImageFileWithBase64(noteId, split[2], split[1]);
+                  final block = BlockEmbed.custom(ImageBlockEmbed(file.path));
+                  delta.insert(block.toJson());
+                }
+              }
+              break;
+            case "video":
+              if (value is String) {
+                var split = value.split(";");
+                if (split.length > 2) {
+                  // !BASE64;mp4;abc.....
+                  final file = await createdVideoFileWithBase64(noteId, split[2], split[1]);
+                  final block = BlockEmbed.custom(VideoBlockEmbed(file.path));
+                  delta.insert(block.toJson());
+                }
+              }
+              break;
+            case "audio":
+              if (value is String) {
+                var split = value.split(";");
+                if (split.length > 2) {
+                  // !BASE64;mp3;abc.....
+                  final file = await createdAudioFileWithBase64(noteId, split[2], split[1]);
+                  final block = BlockEmbed.custom(AudioBlockEmbed(file.path));
+                  delta.insert(block.toJson());
+                }
+              }
+              break;
+            case "table":
+              final id = ref.read(tablesProvider.notifier).generatedId();
+              ref.read(tablesProvider.notifier).insertTable(id, TableData.fromMap(item));
+              BlockEmbed blockEmbed = BlockEmbed.custom(NoteTableBlockEmbed(id));
+              delta.insert(blockEmbed.toJson());
+              break;
+            case "divider":
+              BlockEmbed blockEmbed = BlockEmbed.custom(DividerBlockEmbed(""));
+              delta.insert(blockEmbed.toJson());
+              break;
+          // TODO: Add support for file
+            default:
+              delta.insert(value, item["style"]);
+              break;
+          }
+        }
+
+        document.compose(delta, ChangeSource.local);
+      }
+    } catch (_) {}
+  }
+
+  void importMarkdown(String fileContent) {
+    final mdDocument = md.Document(encodeHtml: false);
+    final mdToDelta = MarkdownToDelta(markdownDocument: mdDocument);
+    final convertedDelta = mdToDelta.convert(fileContent);
+
+    final delta = Delta();
+    for(var item in convertedDelta.toList()) {
+      final value = item.value;
+      if(value is Map<String, dynamic>) {
+        final imageSrc = value["image"];
+        if(imageSrc is String) {
+          if(imageSrc.startsWith("https://")) {
+            // TODO: Download external image URLs and embed them into the note
+          }
+        }
+      }
+      else {
+        delta.insert(item.value, item.attributes);
+      }
+    }
+
+    document.compose(delta, ChangeSource.local);
+  }
+}
