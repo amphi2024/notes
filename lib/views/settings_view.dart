ee75b89ed5bde5bd37c32cf8a5a7958d9429f012
siar1234 <siar019283@gmail.com>
2026-01-25 17:49:15 +0900

    fix: adjust padding for linux csd settings

--- lib/views/settings_view.dart
+++ lib/views/settings_view.dart
@@ -106,58 +106,64 @@ class _SettingsViewState extends ConsumerState<SettingsView> {
               }),
           Visibility(
               visible: Platform.isLinux,
-              child: Row(
-                children: [
-                  Text("Window Controls Style"),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 8.0),
-                    child: DropdownButton<String?>(
-                        value: appSettings.windowControlsStyle,
-                        items: [
-                          DropdownMenuItem(value: "yaru", child: Text("Yaru")),
-                          DropdownMenuItem(value: "arc", child: Text("Arc")),
-                          DropdownMenuItem(
-                              value: "breeze", child: Text("Breeze")),
-                          DropdownMenuItem(
-                              value: "elementary", child: Text("Elementary")),
-                          DropdownMenuItem(
-                              value: "flatRemix", child: Text("Flat Remix")),
-                          DropdownMenuItem(
-                              value: "materia", child: Text("Materia")),
-                          DropdownMenuItem(
-                              value: "nordic", child: Text("Nordic")),
-                          DropdownMenuItem(value: "pop", child: Text("Pop")),
-                          DropdownMenuItem(value: "unity", child: Text("Unity")),
-                          DropdownMenuItem(value: "vimix", child: Text("Vimix")),
-                          DropdownMenuItem(
-                              value: "osxarc", child: Text("OSx Arc")),
-                          DropdownMenuItem(value: null, child: Text("Adwaita"))
-                        ],
-                        onChanged: (value) {
-                          mainScreenKey.currentState?.setState(() {
-                            appSettings.windowControlsStyle = value;
-                          });
-                          setState(() {});
-                        }),
-                  )
-                ],
+              child: Padding(
+                padding: const EdgeInsets.only(left: 10.0, right: 10),
+                child: Row(
+                  children: [
+                    Text("Window Controls Style"),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 8.0),
+                      child: DropdownButton<String?>(
+                          value: appSettings.windowControlsStyle,
+                          items: [
+                            DropdownMenuItem(value: "yaru", child: Text("Yaru")),
+                            DropdownMenuItem(value: "arc", child: Text("Arc")),
+                            DropdownMenuItem(
+                                value: "breeze", child: Text("Breeze")),
+                            DropdownMenuItem(
+                                value: "elementary", child: Text("Elementary")),
+                            DropdownMenuItem(
+                                value: "flatRemix", child: Text("Flat Remix")),
+                            DropdownMenuItem(
+                                value: "materia", child: Text("Materia")),
+                            DropdownMenuItem(
+                                value: "nordic", child: Text("Nordic")),
+                            DropdownMenuItem(value: "pop", child: Text("Pop")),
+                            DropdownMenuItem(value: "unity", child: Text("Unity")),
+                            DropdownMenuItem(value: "vimix", child: Text("Vimix")),
+                            DropdownMenuItem(
+                                value: "osxarc", child: Text("OSx Arc")),
+                            DropdownMenuItem(value: null, child: Text("Adwaita"))
+                          ],
+                          onChanged: (value) {
+                            mainScreenKey.currentState?.setState(() {
+                              appSettings.windowControlsStyle = value;
+                            });
+                            setState(() {});
+                          }),
+                    )
+                  ],
+                ),
               )),
           Visibility(
               visible: Platform.isLinux,
-              child: Row(
-                children: [
-                  Text("Window Controls on Left"),
-                  Checkbox(
-                      value: appSettings.windowButtonsOnLeft,
-                      onChanged: (value) {
-                        if (value != null) {
-                          mainScreenKey.currentState?.setState(() {
-                            appSettings.windowButtonsOnLeft = value;
-                          });
-                          setState(() {});
-                        }
-                      })
-                ],
+              child: Padding(
+                padding: const EdgeInsets.only(left: 10.0, right: 10),
+                child: Row(
+                  children: [
+                    Text("Window Controls on Left"),
+                    Checkbox(
+                        value: appSettings.windowButtonsOnLeft,
+                        onChanged: (value) {
+                          if (value != null) {
+                            mainScreenKey.currentState?.setState(() {
+                              appSettings.windowButtonsOnLeft = value;
+                            });
+                            setState(() {});
+                          }
+                        })
+                  ],
+                ),
               ))
         ]);
   }
